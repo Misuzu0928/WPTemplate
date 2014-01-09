e0e5bf096627b543adb9fdb5f031e5c9da76ae83
Misuzu0928 <una-mela@ae.auone-net.jp>
2014-01-09 12:27:00 +0900

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,16 @@
+<!-- footer-->
+
+<img src="<?php bloginfo('stylesheet_directory'); ?>/image/touka.gif" title="ここからフッターです。" alt="ここからフッターです。">
+<div id="footer">
+<p id="copyright">
+	<small>Copyright &copy; <?php echo date_i18n('Y'); ?>
+	<a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a>
+	All right reserbed.</small>
+</p>
+
+</div>
+</div>
+
+<?php wp_footer(); ?>
+</body>
+</html>
